ef3772c02a4cd408ee343c6962263791eb6fa456
Oscar Caceres Paredes <oscar@MacBook-Air-de-Oscar.local>
2015-03-05 01:45:24 -0300

    angular vistas terminadas

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,12 @@
      $http.get('data/topTwitter.json').success(function(data) {
        $scope.topTwitter = data;
      });
+     $http.get('data/topFacebook.json').success(function(data) {
+       $scope.topFacebook = data;
+     });
+     $http.get('data/topTrend.json').success(function(data) {
+       $scope.topTrend = data;
+     });
    }
    function TopFacebookCtrl($scope, $http) {
      $http.get('data/topFacebook.json').success(function(data) {
@@ -16,6 +22,7 @@
      $http.get('data/topTrend.json').success(function(data) {
        $scope.topTrend = data;
      });
+
    }
    angular.module('prisma.controllers').controller('TopTwitterCtrl',TopTwitterCtrl);
    angular.module('prisma.controllers').controller('TopFacebookCtrl',TopFacebookCtrl);
